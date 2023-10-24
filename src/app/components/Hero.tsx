ab03723d29216fe0947aac55657e70bfb448dac0
ian <ian@harken.cc>
2023-10-24 17:34:43 +0800

    update

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className="flex mx-auto my-0 flex-col justify-center min-h-screen w-full">
       <motion.div
-        className=" font-mono"
+        className=" font-mono text-gray-500"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{
@@ -28,7 +28,7 @@ const Hero = () => {
           delay: 2.2,
         }}
       >
-        <h1 className="font-semibold text-[clamp(40px,8vw,80px)] text-gray-500">Ian Monsanto</h1>
+        <h1 className="font-semibold text-[clamp(40px,8vw,80px)] ">Ian Monsanto</h1>
       </motion.div>
       <motion.div
         className="mt-3"
@@ -40,7 +40,7 @@ const Hero = () => {
           delay: 2.4,
         }}
       >
-        <h1 className="font-semibold leading-[1.1] text-[clamp(40px,8vw,80px)]">I build web and mobile apps.</h1>
+        <h1 className="font-semibold leading-[1.1] text-gray-500 text-[clamp(40px,8vw,80px)]">I build web and mobile apps.</h1>
       </motion.div>
       <motion.div
         className="mt-5"
@@ -52,7 +52,7 @@ const Hero = () => {
           delay: 2.6,
         }}
       >
-        <p className="text-gray-500 max-w-[545px]">
+        <p className="text-gray-300 max-w-[545px]">
           I&apos;m a software developer based in the Philippines, specializing in
           building websites and mobile applications, and everything
           in between.
