5108680b348b49c566975654255a9917ed03069e
ian <ian@harken.cc>
2023-10-26 17:36:33 +0800

    update

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -24,18 +24,21 @@ const Experience = () => {
         </h2>
         <div className="w-full h-[1px] ml-2 bg-gray-600" />
       </div>
-      <div className="grid grid-cols-[repeat(10,1fr)] w-full rounded-md bg-transparent">
+      <div
+        className={`opacity-0 duration-1000 delay-200 ${
+          isInview ? "opacity-100" : ""
+        } grid grid-cols-[repeat(10,1fr)] w-full rounded-md bg-transparent`}
+      >
         <div className="col-[1/-1] sm:col-[1/8] row-[1/2] transition-all ease-in duration-500 z-20 p-10">
           <div className="text-neutral-500">Featured Project</div>
           <h3 className="text-3xl">Project Name</h3>
           <div className="sm:bg-neutral-800 rounded-xl sm:p-3 my-8">
-
-          <p className="text-neutral-400">
-            A web app for visualizing personalized Spotify data. View your top
-            artists, top tracks, recently played tracks, and detailed audio
-            information about each track. Create and save new playlists of
-            recommended tracks based on your existing playlists and more.
-          </p>
+            <p className="text-neutral-400">
+              A web app for visualizing personalized Spotify data. View your top
+              artists, top tracks, recently played tracks, and detailed audio
+              information about each track. Create and save new playlists of
+              recommended tracks based on your existing playlists and more.
+            </p>
           </div>
           <ul className="flex gap-4">
             <li>React</li>
