ba00dbdd10437e40eb8a4a0fdd32a2d54f552a3d
ian <ian@harken.cc>
2023-10-26 17:31:30 +0800

    update page padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Experience from "./components/Experience";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-12">
+    <main className="flex min-h-screen flex-col items-center justify-center px-6">
       <Hero />
       <About />
       <Experience />
